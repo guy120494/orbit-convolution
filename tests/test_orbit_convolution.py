26c1a407ff5f95fb7486051ba0efcfb0ce2cea79
guy <guy120494@gmail.com>
2020-12-08 14:11:21 +0200

    Added more test cases

--- tests/test_orbit_convolution.py
+++ tests/test_orbit_convolution.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 
@@ -8,7 +7,7 @@ from layers.OrbitConvolution import OrbitConvolution
 
 class OrbitConvolutionTest(tf.test.TestCase):
 
-    def test_equivariance_in_x_axis(self):
+    def test_equivariance_in_y_axis(self):
         layer = OrbitConvolution(kernel_size=3, num_filters=1)
         x_train, _, _, _ = get_mnist_data()
         sample = x_train[0]
@@ -33,6 +32,35 @@ class OrbitConvolutionTest(tf.test.TestCase):
         sample1 = np.roll(sample, (0, 2), axis=(1, 2))
         sample2 = np.roll(sample, (0, -2), axis=(1, 2))
 
+        result1 = layer(tf.convert_to_tensor(sample1))
+        result2 = layer(tf.convert_to_tensor(sample2))
+
+        self.assertAllClose(result1, result2, message="Not invariant")
+
+    def test_equivariance_in_x_axis(self):
+        layer = OrbitConvolution(kernel_size=3, num_filters=1, axis=1)
+        x_train, _, _, _ = get_mnist_data()
+        sample = x_train[0]
+        sample = np.reshape(sample, [1, *sample.shape])
+        sample = tf.convert_to_tensor(sample)
+
+        sample1 = np.roll(sample, (0, 2), axis=(1, 2))
+        sample2 = np.roll(sample, (0, -2), axis=(1, 2))
+
+        result1 = layer(tf.convert_to_tensor(sample1))
+        result2 = layer(tf.convert_to_tensor(sample2))
+
+        self.assertAllClose(tf.roll(result1, (0, -4), axis=(1, 2)), result2, message="Not equivariant")
+
+    def test_invariance_in_y_axis(self):
+        layer = OrbitConvolution(kernel_size=3, num_filters=1, axis=1)
+        x_train, _, _, _ = get_mnist_data()
+        sample = x_train[0]
+        sample = np.reshape(sample, [1, *sample.shape])
+        sample = tf.convert_to_tensor(sample)
+
+        sample1 = np.roll(sample, (2, 0), axis=(1, 2))
+        sample2 = np.roll(sample, (-2, 0), axis=(1, 2))
 
         result1 = layer(tf.convert_to_tensor(sample1))
         result2 = layer(tf.convert_to_tensor(sample2))
