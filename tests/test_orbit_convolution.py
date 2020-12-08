66f8ec3561888130f3b15f4f12a458c5cced8b5c
guy <guy120494@gmail.com>
2020-12-08 15:25:02 +0200

    Fixed tests names

--- tests/test_orbit_convolution.py
+++ tests/test_orbit_convolution.py
@@ -7,7 +7,7 @@ from layers.OrbitConvolution import OrbitConvolution
 
 class OrbitConvolutionTest(tf.test.TestCase):
 
-    def test_equivariance_in_y_axis(self):
+    def test_invariance_in_x_axis(self):
         layer = OrbitConvolution(kernel_size=3, num_filters=1)
         x_train, _, _, _ = get_mnist_data()
         sample = x_train[0]
@@ -17,12 +17,12 @@ class OrbitConvolutionTest(tf.test.TestCase):
         sample1 = np.roll(sample, (2, 0), axis=(1, 2))
         sample2 = np.roll(sample, (-2, 0), axis=(1, 2))
 
-        result1 = layer(tf.convert_to_tensor(sample1))
-        result2 = layer(tf.convert_to_tensor(sample2))
+        result1 = tf.squeeze(layer(tf.convert_to_tensor(sample1)))
+        result2 = tf.squeeze(layer(tf.convert_to_tensor(sample2)))
 
-        self.assertAllClose(tf.roll(result1, (-4, 0), axis=(1, 2)), result2, message="Not equivariant")
+        self.assertAllClose(result1, result2, msg="Not equivariant")
 
-    def test_invariance_in_x_axis(self):
+    def test_equivariance_in_y_axis(self):
         layer = OrbitConvolution(kernel_size=3, num_filters=1)
         x_train, _, _, _ = get_mnist_data()
         sample = x_train[0]
@@ -32,25 +32,10 @@ class OrbitConvolutionTest(tf.test.TestCase):
         sample1 = np.roll(sample, (0, 2), axis=(1, 2))
         sample2 = np.roll(sample, (0, -2), axis=(1, 2))
 
-        result1 = layer(tf.convert_to_tensor(sample1))
-        result2 = layer(tf.convert_to_tensor(sample2))
+        result1 = tf.squeeze(layer(tf.convert_to_tensor(sample1)))
+        result2 = tf.squeeze(layer(tf.convert_to_tensor(sample2)))
 
-        self.assertAllClose(result1, result2, message="Not invariant")
-
-    def test_equivariance_in_x_axis(self):
-        layer = OrbitConvolution(kernel_size=3, num_filters=1, axis=1)
-        x_train, _, _, _ = get_mnist_data()
-        sample = x_train[0]
-        sample = np.reshape(sample, [1, *sample.shape])
-        sample = tf.convert_to_tensor(sample)
-
-        sample1 = np.roll(sample, (0, 2), axis=(1, 2))
-        sample2 = np.roll(sample, (0, -2), axis=(1, 2))
-
-        result1 = layer(tf.convert_to_tensor(sample1))
-        result2 = layer(tf.convert_to_tensor(sample2))
-
-        self.assertAllClose(tf.roll(result1, (0, -4), axis=(1, 2)), result2, message="Not equivariant")
+        self.assertAllClose(tf.roll(result1, -4, axis=0), result2, msg="Not invariant")
 
     def test_invariance_in_y_axis(self):
         layer = OrbitConvolution(kernel_size=3, num_filters=1, axis=1)
@@ -59,10 +44,29 @@ class OrbitConvolutionTest(tf.test.TestCase):
         sample = np.reshape(sample, [1, *sample.shape])
         sample = tf.convert_to_tensor(sample)
 
+        sample1 = np.roll(sample, (0, 2), axis=(1, 2))
+        sample2 = np.roll(sample, (0, -2), axis=(1, 2))
+
+        result1 = tf.squeeze(layer(tf.convert_to_tensor(sample1)))
+        result2 = tf.squeeze(layer(tf.convert_to_tensor(sample2)))
+
+        self.assertAllClose(result1, result2, msg="Not equivariant")
+
+    def test_equivariance_in_x_axis(self):
+        layer = OrbitConvolution(kernel_size=3, num_filters=1, axis=1)
+        x_train, _, _, _ = get_mnist_data()
+        sample = x_train[0]
+        sample = np.reshape(sample, [1, *sample.shape])
+        sample = tf.convert_to_tensor(sample)
+
         sample1 = np.roll(sample, (2, 0), axis=(1, 2))
         sample2 = np.roll(sample, (-2, 0), axis=(1, 2))
 
-        result1 = layer(tf.convert_to_tensor(sample1))
-        result2 = layer(tf.convert_to_tensor(sample2))
+        result1 = tf.squeeze(layer(tf.convert_to_tensor(sample1)))
+        result2 = tf.squeeze(layer(tf.convert_to_tensor(sample2)))
 
-        self.assertAllClose(result1, result2, message="Not invariant")
+        self.assertAllClose(tf.roll(result1, -4, axis=0), result2, msg="Not invariant")
+
+
+if __name__ == '__main__':
+    tf.test.main()
