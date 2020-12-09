d181c94cadfd84490518632d51e83dd7c645ee39
guy <guy120494@gmail.com>
2020-12-09 14:46:46 +0200

    The benchmark modelˆ

--- models/BasicModel.py
+++ models/BasicModel.py
@@ -1,17 +1,28 @@
+from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Dropout, Dense, Flatten
 from tensorflow.python.keras.models import Model
-import tensorflow as tf
-from layers.OrbitConvolution import OrbitConvolution
 
 
 class BasicModel(Model):
+
     def __init__(self):
         super(BasicModel, self).__init__()
-        self.first_layer = OrbitConvolution(kernel_size=3, num_filters=2)
-        self.flatten = tf.keras.layers.Flatten()
-        self.dense = tf.keras.layers.Dense(10, activation='softmax')
+        self.first_cnn_layer = None
+        self.second_cnn_layer = Conv2D(64, kernel_size=(3, 3), activation='relu')
+        self.max_pooling = MaxPooling2D(pool_size=(2, 2))
+        self.first_dropout = Dropout(0.25)
+        self.flatten = Flatten()
+        self.first_dense = Dense(128, activation='relu')
+        self.second_dropout = Dropout(0.5)
+        self.dense = Dense(10, activation='softmax')
+
+    def build(self, input_shape):
+        self.first_cnn_layer = Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape)
 
     def call(self, inputs, training=None, mask=None):
-        x = self.first_layer(inputs)
+        x = self.first_cnn_layer(inputs)
+        x = self.second_cnn_layer(x)
+        x = self.max_pooling(x)
+        x = self.first_dropout(x, training=training)
         x = self.flatten(x)
         return self.dense(x)
 
