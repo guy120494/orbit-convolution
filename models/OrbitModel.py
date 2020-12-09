adc1bd77fcfdd0bc23cb596293ad95d65b93eb58
guy <guy120494@gmail.com>
2020-12-09 16:36:36 +0200

    Comment out dropout

--- models/OrbitModel.py
+++ models/OrbitModel.py
@@ -12,10 +12,10 @@ class OrbitModel(Model):
         self.first_cnn_layer = None
         self.second_cnn_layer = OrbitConvolution(kernel_size=3, num_filters=64, axis=1)
         self.max_pooling = MaxPooling1D(pool_size=2)
-        # self.first_dropout = Dropout(0.25)
+        self.first_dropout = Dropout(0.25)
         self.flatten = Flatten()
         self.first_dense = Dense(128, activation='relu')
-        # self.second_dropout = Dropout(0.5)
+        self.second_dropout = Dropout(0.5)
         self.second_dense = Dense(10, activation='softmax')
 
     def build(self, input_shape):
@@ -26,10 +26,10 @@ class OrbitModel(Model):
         x = self.second_cnn_layer(x)
         x = tf.nn.relu(x)
         x = self.max_pooling(x)
-        x = self.first_dropout(x, training=training)
+        # x = self.first_dropout(x, training=training)
         x = self.flatten(x)
         x = self.first_dense(x)
-        x = self.second_dropout(x, training=training)
+        # x = self.second_dropout(x, training=training)
         return self.second_dense(x)
 
     def get_config(self):
