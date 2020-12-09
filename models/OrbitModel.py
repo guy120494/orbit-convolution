a55b8b3087484cc88b2da67bec44b82b5eecdfd4
guy <guy120494@gmail.com>
2020-12-09 18:10:55 +0200

    Orbit convolution is the first layer

--- models/OrbitModel.py
+++ models/OrbitModel.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.python.keras.layers import Conv2D, Dropout, Dense, Flatten, MaxPooling1D
+from tensorflow.python.keras.layers import Dropout, Dense, Flatten, MaxPooling1D, Conv1D
 from tensorflow.python.keras.models import Model
 
 from layers.OrbitConvolution import OrbitConvolution
@@ -9,17 +9,14 @@ class OrbitModel(Model):
 
     def __init__(self, axis: int = 0):
         super(OrbitModel, self).__init__()
-        self.first_cnn_layer = None
-        self.second_cnn_layer = OrbitConvolution(kernel_size=3, num_filters=64, axis=axis)
+        self.first_cnn_layer = OrbitConvolution(num_filters=32, kernel_size=3, axis=axis)
+        self.second_cnn_layer = Conv1D(64, kernel_size=3)
         self.max_pooling = MaxPooling1D(pool_size=2)
         self.first_dropout = Dropout(0.25)
         self.flatten = Flatten()
         self.first_dense = Dense(128, activation='relu')
         self.second_dropout = Dropout(0.5)
         self.second_dense = Dense(10, activation='softmax')
-
-    def build(self, input_shape):
-        self.first_cnn_layer = Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape)
 
     def call(self, inputs, training=None, mask=None):
         x = self.first_cnn_layer(inputs)
