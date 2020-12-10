b783f462a5da01636ec5bd5d1df2b4da5f572470
guy <guy120494@gmail.com>
2020-12-10 09:57:11 +0200

    Changed number of parameters

--- models/OrbitModel.py
+++ models/OrbitModel.py
@@ -10,16 +10,16 @@ class OrbitModel(Model):
     def __init__(self, axis: int = 0):
         super(OrbitModel, self).__init__()
         self.first_cnn_layer = None
-        self.second_cnn_layer = OrbitConvolution(name="orbit", kernel_size=3, num_filters=1024, axis=axis)
+        self.second_cnn_layer = OrbitConvolution(name="orbit", kernel_size=3, num_filters=256, axis=axis)
         self.max_pooling = MaxPooling1D(pool_size=2)
         # self.first_dropout = Dropout(0.25)
         self.flatten = Flatten()
-        self.first_dense = Dense(128, activation='relu')
+        self.first_dense = Dense(900, activation='relu')
         # self.second_dropout = Dropout(0.5)
         self.second_dense = Dense(10, activation='softmax')
 
     def build(self, input_shape):
-        self.first_cnn_layer = Conv2D(name="convolution", filters=1024, kernel_size=(3, 3), activation='relu',
+        self.first_cnn_layer = Conv2D(name="convolution", filters=128, kernel_size=(3, 3), activation='relu',
                                       input_shape=input_shape)
 
     def call(self, inputs, training=None, mask=None):
