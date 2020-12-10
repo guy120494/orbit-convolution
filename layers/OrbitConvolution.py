25382e7918605387c05e05e9e651f332b9ea919c
guy <guy120494@gmail.com>
2020-12-10 14:56:53 +0200

    OrbitConvolution is now divided to three classesˆ

--- layers/OrbitConvolution.py
+++ layers/OrbitConvolution.py
@@ -1,25 +1,70 @@
 import tensorflow as tf
-import tensorflow.keras as keras
 from tensorflow.keras.layers import Layer
 
 
-class OrbitConvolution(Layer):
+class OrbitSumConvolution(Layer):
     def __init__(self, kernel_size: int, num_filters: int, axis: int = 0, **kwargs):
         self.axis = axis
         self.kernel_size = kernel_size
         self.num_filters = num_filters
         self.kernel = None
-        super(OrbitConvolution, self).__init__(**kwargs)
+        super(OrbitSumConvolution, self).__init__(**kwargs)
 
     def build(self, input_shape):
         shape = tf.TensorShape([self.kernel_size, input_shape[-1], self.num_filters])
 
         self.kernel = self.add_weight(name='kernel', shape=shape,
                                       initializer='glorot_uniform')
-        super(OrbitConvolution, self).build(input_shape)
+        super(OrbitSumConvolution, self).build(input_shape)
 
     def call(self, x, **kwargs):
         x = tf.math.reduce_sum(x, axis=self.axis + 1)
+        return tf.nn.conv1d(x, self.kernel, padding='SAME', stride=1)
+
+    def compute_output_shape(self, input_shape):
+        return self.kernel_size, input_shape[-(self.axis + 1)], self.num_filters
+
+
+class OrbitMaxConvolution(Layer):
+    def __init__(self, kernel_size: int, num_filters: int, axis: int = 0, **kwargs):
+        self.axis = axis
+        self.kernel_size = kernel_size
+        self.num_filters = num_filters
+        self.kernel = None
+        super(OrbitMaxConvolution, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        shape = tf.TensorShape([self.kernel_size, input_shape[-1], self.num_filters])
+
+        self.kernel = self.add_weight(name='kernel', shape=shape,
+                                      initializer='glorot_uniform')
+        super(OrbitMaxConvolution, self).build(input_shape)
+
+    def call(self, x, **kwargs):
+        x = tf.math.reduce_max(x, axis=self.axis + 1)
+        return tf.nn.conv1d(x, self.kernel, padding='SAME', stride=1)
+
+    def compute_output_shape(self, input_shape):
+        return self.kernel_size, input_shape[-(self.axis + 1)], self.num_filters
+
+
+class OrbitMeanConvolution(Layer):
+    def __init__(self, kernel_size: int, num_filters: int, axis: int = 0, **kwargs):
+        self.axis = axis
+        self.kernel_size = kernel_size
+        self.num_filters = num_filters
+        self.kernel = None
+        super(OrbitMeanConvolution, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        shape = tf.TensorShape([self.kernel_size, input_shape[-1], self.num_filters])
+
+        self.kernel = self.add_weight(name='kernel', shape=shape,
+                                      initializer='glorot_uniform')
+        super(OrbitMeanConvolution, self).build(input_shape)
+
+    def call(self, x, **kwargs):
+        x = tf.math.reduce_mean(x, axis=self.axis + 1)
         return tf.nn.conv1d(x, self.kernel, padding='SAME', stride=1)
 
     def compute_output_shape(self, input_shape):
--- tests/test_orbit_convolution.py
+++ tests/test_orbit_convolution.py
@@ -2,13 +2,13 @@ import numpy as np
 import tensorflow as tf
 
 from datasets.data_sets_fetcher import get_mnist_data
-from layers.OrbitConvolution import OrbitConvolution
+from layers.OrbitConvolution import OrbitSumConvolution
 
 
 class OrbitConvolutionTest(tf.test.TestCase):
 
     def test_invariance_in_x_axis(self):
-        layer = OrbitConvolution(kernel_size=3, num_filters=1)
+        layer = OrbitSumConvolution(kernel_size=3, num_filters=1)
         x_train, _, _, _ = get_mnist_data()
         sample = x_train[0]
         sample = np.reshape(sample, [1, *sample.shape])
@@ -23,7 +23,7 @@ class OrbitConvolutionTest(tf.test.TestCase):
         self.assertAllClose(result1, result2, msg="Not invariant")
 
     def test_equivariance_in_y_axis(self):
-        layer = OrbitConvolution(kernel_size=3, num_filters=1)
+        layer = OrbitSumConvolution(kernel_size=3, num_filters=1)
         x_train, _, _, _ = get_mnist_data()
         sample = x_train[0]
         sample = np.reshape(sample, [1, *sample.shape])
@@ -38,7 +38,7 @@ class OrbitConvolutionTest(tf.test.TestCase):
         self.assertAllClose(tf.roll(result1, -4, axis=0), result2, msg="Not equivariant")
 
     def test_invariance_in_y_axis(self):
-        layer = OrbitConvolution(kernel_size=3, num_filters=1, axis=1)
+        layer = OrbitSumConvolution(kernel_size=3, num_filters=1, axis=1)
         x_train, _, _, _ = get_mnist_data()
         sample = x_train[0]
         sample = np.reshape(sample, [1, *sample.shape])
@@ -53,7 +53,7 @@ class OrbitConvolutionTest(tf.test.TestCase):
         self.assertAllClose(result1, result2, msg="Not invariant")
 
     def test_equivariance_in_x_axis(self):
-        layer = OrbitConvolution(kernel_size=3, num_filters=1, axis=1)
+        layer = OrbitSumConvolution(kernel_size=3, num_filters=1, axis=1)
         x_train, _, _, _ = get_mnist_data()
         sample = x_train[0]
         sample = np.reshape(sample, [1, *sample.shape])
