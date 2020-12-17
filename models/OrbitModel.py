0eca872cc0976d9941694bd3580e9153da8ce74d
guy <guy120494@gmail.com>
2020-12-17 12:05:31 +0200

    Fix bug

--- models/OrbitModel.py
+++ models/OrbitModel.py
@@ -23,11 +23,11 @@ class OrbitModel(Model):
         self.axis = axis
         self.first_cnn_layer = None
         self.second_cnn_layer = self.get_orbit_layer()
-        self.max_pooling = MaxPooling1D(pool_size=2)
-        self.first_dropout = Dropout(0.25)
+        # self.max_pooling = MaxPooling1D(pool_size=2)
+        # self.first_dropout = Dropout(0.25)
         self.flatten = Flatten()
         self.first_dense = Dense(128, activation='relu')
-        # self.second_dropout = Dropout(0.5)
+        self.second_dropout = Dropout(0.5)
         self.second_dense = Dense(10, activation='softmax')
 
     def get_orbit_layer(self):
