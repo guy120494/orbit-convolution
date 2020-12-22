2e9c2dae845a405467f39e3d074c13ddaea760b1
guy <guy120494@gmail.com>
2020-12-22 17:43:33 +0200

    BN after second convolution

--- models/OrbitModel.py
+++ models/OrbitModel.py
@@ -54,7 +54,7 @@ class OrbitModel(Model):
         # x = self.bn1(x)
         x = self.second_cnn_layer(x)
         x = self.relu(x)
-        # x = self.bn2(x)
+        x = self.bn2(x)
         # x = self.max_pooling(x)
         # x = self.first_dropout(x, training=training)
         x = self.flatten(x)
