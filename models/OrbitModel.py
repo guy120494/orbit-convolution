9867a2af746beaaaeefc85c6c0e63f00cb48903e
guy <guy120494@gmail.com>
2020-12-24 15:34:41 +0200

    max pooling

--- models/OrbitModel.py
+++ models/OrbitModel.py
@@ -55,11 +55,11 @@ class OrbitModel(Model):
         x = self.second_cnn_layer(x)
         x = self.relu(x)
         # x = self.bn2(x)
-        # x = self.max_pooling(x)
+        x = self.max_pooling(x)
         # x = self.first_dropout(x, training=training)
         x = self.flatten(x)
         x = self.first_dense(x)
-        x = self.second_dropout(x, training=training)
+        # x = self.second_dropout(x, training=training)
         return self.second_dense(x)
 
     def get_config(self):
